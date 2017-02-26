3f859f59441921f6a7a6b33598430d488f46a909
TedCleborne <ted.cleborne@gmail.com>
2017-02-26 17:11:25 -0500

    Long Due Update
    Update from 2/26

--- js/my.js
+++ js/my.js
@@ -1,3 +1,12 @@
-               $('a.hamburger-menu').click(function() { 
-                  $('nav.main-menu, .hide').toggleClass('show');
-                });
+$('a.hamburger-menu').click(function() { 
+   $('nav.main-menu, .hide').toggleClass('show');
+   });
+
+
+
+$("button").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".second").offset().top},
+        'slow');
+});
+
